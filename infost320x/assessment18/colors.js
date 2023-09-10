166240df8e1fa0eaf8fdcb6b6c1174816536dd4b
Christopher Maio <maiocj@gmail.com>
2023-09-10 17:33:17 -0400

    [INFOST320X] Assessment 18

---
+++ infost320x/assessment18/colors.js
@@ -0,0 +1,22 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Add listener to the `text_color` radio buttons
+  document.getElementsByName('text_color').forEach((element) => {
+    element.addEventListener('change', (event) => {
+      document.body.style.color = event.target.value;
+    });
+  });
+
+  // Add listeners to the `background_color` radio buttons
+  document.getElementsByName('background_color').forEach((element) => {
+    element.addEventListener('change', (event) => {
+      document.body.style.backgroundColor = event.target.value;
+    });
+  });
+
+  // Add listener onto the `first_name_entry` form
+  document.getElementById('first_name_entry').addEventListener('submit', (event) => {
+    event.preventDefault();
+    alert(`Hello, ${event.target.first_name.value}! Thank you for visiting!`);
+  });
+});
