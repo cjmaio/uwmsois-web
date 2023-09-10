3d281c8d10d519c1ff22f72aefae0ebf7b657cc8
Christopher Maio <maiocj@gmail.com>
2023-09-10 19:06:41 -0400

    [INFOST320X] Assessment 22

--- infost320x/assessment22/functions.js
+++ infost320x/assessment22/functions.js
@@ -11,48 +11,48 @@ $(document).ready(function () {
 
   $("#thumb1").bind("click", function () {
     $("#mainphoto").hide()
-    $("#caption").html("Archway in the park");
-    $("#mainphoto").attr("src", "images/image1.jpg").fadeIn();
+    $("#caption").html("Two cats laying together");
+    $("#mainphoto").attr("src", "images/cats-1.webp").fadeIn();
     $(".smallimg").attr("border", "0");
     $("#thumb1").attr("border", "2");
   });
 
   $("#thumb2").bind("click", function () {
     $("#mainphoto").hide()
-    $("#caption").html("Busy bee");
-    $("#mainphoto").attr("src", "images/image2.jpg").fadeIn();
+    $("#caption").html("A cat in the grass");
+    $("#mainphoto").attr("src", "images/cats-2.jpg").fadeIn();
     $(".smallimg").attr("border", "0");
     $("#thumb2").attr("border", "2");
   });
 
   $("#thumb3").bind("click", function () {
     $("#mainphoto").hide()
-    $("#caption").html("Munich Strasse");
-    $("#mainphoto").attr("src", "images/image3.jpg").fadeIn();
+    $("#caption").html("A cat looking stressed");
+    $("#mainphoto").attr("src", "images/cats-3.jpg").fadeIn();
     $(".smallimg").attr("border", "0");
     $("#thumb3").attr("border", "2");
   });
 
   $("#thumb4").bind("click", function () {
     $("#mainphoto").hide()
-    $("#caption").html("Munich Gardens");
-    $("#mainphoto").attr("src", "images/image4.jpg").fadeIn();
+    $("#caption").html("A cat in very short grass");
+    $("#mainphoto").attr("src", "images/cats-4.jpg").fadeIn();
     $(".smallimg").attr("border", "0");
     $("#thumb4").attr("border", "2");
   });
 
   $("#thumb5").bind("click", function () {
     $("#mainphoto").hide()
-    $("#caption").html("View from above");
-    $("#mainphoto").attr("src", "images/image5.jpg").fadeIn();
+    $("#caption").html("A cat with a single orange brain cell sleeping");
+    $("#mainphoto").attr("src", "images/cats-5.png").fadeIn();
     $(".smallimg").attr("border", "0");
     $("#thumb5").attr("border", "2");
   });
 
   $("#thumb6").bind("click", function () {
     $("#mainphoto").hide()
-    $("#caption").html("View of the city");
-    $("#mainphoto").attr("src", "images/image6.jpg").fadeIn();
+    $("#caption").html("A cat with a single orange brain cell screaming");
+    $("#mainphoto").attr("src", "images/cats-6.png").fadeIn();
     $(".smallimg").attr("border", "0");
     $("#thumb6").attr("border", "2");
   });
